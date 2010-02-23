499d1844dfc220ac72ccfe56d204187f8cb67eda
Chad Hutchins <hutchins.chad@gmail.com>
2010-02-23 16:20:17 -0600

    Cached  var for imporoved performance, added a closure to the plugin to play nice with jQuery.noConflict(), removed empty readme file. Thanks to github.com/pifantastic for the suggestions.

--- src/jquery.moveb.js
+++ src/jquery.moveb.js
@@ -6,26 +6,31 @@
  *
  */
 
-$.expr[':'].around = function(obj,index,meta,stack) {
-	var obj_pos = $(obj).position();
-	var obj_width = $(obj).width();
-	var obj_height = $(obj).height();
-	
-	var obj_min_left = obj_pos.left;
-	var obj_max_left = obj_min_left + obj_width;
-	var obj_min_top = obj_pos.top;
-	var obj_max_top = obj_min_top + obj_height;
+(function($) {
 
-	// get params
-	var values = meta[3].split(",");
+	$.expr[':'].around = function(obj,index,meta,stack) {
+		var $obj = $(obj);
+		var obj_pos = $obj.position();
+		var obj_width = $obj.width();
+		var obj_height = $obj.height();
 	
-	var click_left = parseInt(values[1]);
-	var click_top = parseInt(values[0]);	
+		var obj_min_left = obj_pos.left;
+		var obj_max_left = obj_min_left + obj_width;
+		var obj_min_top = obj_pos.top;
+		var obj_max_top = obj_min_top + obj_height;
 
-	return (
-		click_left >= obj_min_left &&
-		click_left <= obj_max_left &&
-		click_top >= obj_min_top &&
-		click_top <= obj_max_top
-	);
-}
+		// get params
+		var values = meta[3].split(",");
+	
+		var click_left = parseInt(values[1]);
+		var click_top = parseInt(values[0]);	
+
+		return (
+			click_left >= obj_min_left &&
+			click_left <= obj_max_left &&
+			click_top >= obj_min_top &&
+			click_top <= obj_max_top
+		);
+	}
+
+})(jQuery);
