9136163d8bee27a9ff6cc6fc6512bc7146e75492
Chad Hutchins <hutchins.chad@gmail.com>
2010-02-23 21:44:19 -0600

    added .moveb() method to plugin that moves elements out of the way of a particular point

--- README.txt
+++ README.txt
@@ -6,7 +6,7 @@ Copyright (c) 2010 Chad Hutchins. Licensed under the MIT license.
 Overview
 --------
 
-Currently, MoveB is a jQuery plugin that lets you determine what elements contain a particular point on the page. We'll see what it turns into over time... 
+MoveB is a jQuery plugin that can move elements out of the way of a certain point on the page and lets you determine what elements contain a particular point on the page. We'll see what it turns into over time... 
 
 
 Usage
@@ -17,3 +17,12 @@ $(":around(250,500)")
 
 // hide all div elements with the class "holla" that the point 300,300 falls within
 $("div.holla:around(300,300)").hide();
+
+// if the given point 250,250 is within the bounds of any div element with the class "holla", the div gets moved out of the way
+$("div.holla").moveb(250,250);
+
+// same as the previous example, except we set the padding distance to move the elements 100px from the given point
+var settings = {
+	padding: 100
+};
+$("div.holla").moveb(250,250,settings);
--- src/jquery.moveb.js
+++ src/jquery.moveb.js
@@ -8,29 +8,80 @@
 
 (function($) {
 
+	$.fn.moveb = function(left,top,settings) {
+		settings = jQuery.extend({
+			padding: 10
+		}, settings);
+		return this.each(function(){
+			var $obj = $(this);
+			var obj_pos = $obj.position();
+			var obj_width = $obj.width();
+			var obj_height = $obj.height();
+			if (is_point_inside_rectangle(left,top,obj_pos.left,obj_pos.top,obj_width,obj_height)) {
+				var quadrant = get_rectangle_quadrant(left,top,obj_pos.left,obj_pos.top,obj_width,obj_height);
+				if (quadrant==1) {
+					$obj.css({
+						left: left + settings.padding,
+						top: top + settings.padding
+					});
+				} else if (quadrant==2) {
+					$obj.css({
+						left: obj_pos.left - (obj_pos.left + obj_width - left) - settings.padding,
+						top: top + settings.padding
+					});
+				} else if (quadrant==3) {
+					$obj.css({
+						left: left + settings.padding,
+						top: obj_pos.top - (obj_pos.top + obj_height - top) - settings.padding
+					});
+				} else if (quadrant==4) {
+					$obj.css({
+						left: obj_pos.left - (obj_pos.left + obj_width - left) - settings.padding,
+						top: obj_pos.top - (obj_pos.top + obj_height - top) - settings.padding
+					});
+				}
+			}
+		})
+	}
+
 	$.expr[':'].around = function(obj,index,meta,stack) {
 		var $obj = $(obj);
 		var obj_pos = $obj.position();
 		var obj_width = $obj.width();
 		var obj_height = $obj.height();
 	
-		var obj_min_left = obj_pos.left;
-		var obj_max_left = obj_min_left + obj_width;
-		var obj_min_top = obj_pos.top;
-		var obj_max_top = obj_min_top + obj_height;
-
 		// get params
 		var values = meta[3].split(",");
 	
 		var click_left = parseInt(values[1]);
 		var click_top = parseInt(values[0]);	
 
+		return is_point_inside_rectangle(click_left,click_top,obj_pos.left,obj_pos.top,obj_width,obj_height);
+	}
+	
+	function is_point_inside_rectangle(point_left,point_top,obj_left,obj_top,obj_width,obj_height) {
 		return (
-			click_left >= obj_min_left &&
-			click_left <= obj_max_left &&
-			click_top >= obj_min_top &&
-			click_top <= obj_max_top
+			point_left >= obj_left &&
+			point_left <= (obj_left + obj_width) &&
+			point_top >= obj_top &&
+			point_top <= (obj_top + obj_height)
 		);
+	}
+	
+	function get_rectangle_quadrant(point_left,point_top,obj_left,obj_top,obj_width,obj_height) {
+		var half_width = obj_width / 2;
+		var half_height = obj_height / 2;
+		var mid_left = obj_left + half_width;
+		var mid_top = obj_top + half_height;
+		if (point_left<=mid_left && point_top<=mid_top) {
+			return 1;
+		} else if (point_left>mid_left && point_top<=mid_top) {
+			return 2;
+		} else if (point_left<=mid_left && point_top>mid_top) {
+			return 3;
+		} else if (point_left>mid_left && point_top>mid_top) {
+			return 4;
+		}
 	}
 
 })(jQuery);
